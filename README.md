ed2aeda3dc15ef4827d7aa97aa9b13cc5d127d99
Jimb Esser <wasteland@gmail.com>
2018-04-21 08:11:14 -0700

    Initial commit: Fork from turbulenz-playground

---
+++ README.md
@@ -0,0 +1,84 @@
+LD41 Entry by Jimbly
+============================
+
+* Using [Javascript libGlov/GLOV.js framework](https://github.com/Jimbly/turbulenz-playground)
+
+Start with: `npm start` (after running `npm i` once)
+
+TODO
+* Figure out what to do.
+
+Deckbuilding Bullet-hell Scope
+* Build a BHS
+  * avatar (movable with controls for debug)
+  * simple enemy movement and firing patterns
+  * collision detection / health
+  * later: simple powerups - spread, machine gun
+  * later: allow retry/continue upon fail
+* Card-playing while playing BHS
+  * Show hand of cards, with area for rollover description
+  * Click card, click target (or right click/click on-screen button to abort), generate input to BHS
+  * Cards draw into hand at regular intervals up to hand limit
+  * later: Show hand limit (counters on empty slots?)
+* Art
+  * Avatar
+  * Background
+  * Enemies
+  * Bullets
+  * Explosions
+
+Rhythm Tower Defense Scope
+* Build a rhythm game
+  * Just notes/arrows coming down, hitting them, playing simple sound on hit
+  * No music syncing to start with
+  * Want to be able to add more and more layers of patterns to make it more difficult
+* Add TD
+  * Notes have HP, towers do damage to lowest HP, repeat
+* Currency name: "Eighths"
+* Art - go with a simple musical score kind of theme
+  * Notes (simple)
+  * Towers (gun turrets? look like fermatas?)
+  * Sound
+    * Can do simple sounds just to have rhythm in background and drums when notes are hit
+    * Could go crazy with procedural chord progression and guns making music and such
+
+Theme Thoughts
+* Deck building (or just card game?) + Bullet-hell Shooter
+  * probably real-time
+    * draw cards at a fixed rate per second
+    * hand limit to encourage playing cards
+    * can see what's coming for a bit to encourage planning/saving cards
+  * movement cards
+    * "move right"? "move 3" playing a card requires targeting a location?
+  * reaction cards
+  * things like shields, healing, armor, etc
+  * always shooting? maybe cards that change your shoot angle
+  * powerups come down - just one-time use cards to add to your hand, or bonuses like "Draw 2"
+    * or new cards to add to your deck - have to trash something in hand?
+    * or regular powerups like spread/etc
+  * between level upgrading - ship has X slots, and each slot is equipped with an item that provides a set of cards
+    * E.g. thruster choices: "3 move 1 cards" "1 move 3 card" "2 move 1 and 2 shield", etc
+    * Each level just give them an two "interesting choice" upgrades for two slots?
+    * Or, better/more upgrades based on how they did?
+    * Slots also determine other ship factors - Hand size, draw speed, anything else?
+      * Maybe ship hull is hand size and draw speed, as otherwise they're too significant of an advantage to be uncoupled
+* Turn Based + Dogfighting
+* Rhythm + Card Game
+  * card have patterns of moves
+  * pattern is coming down
+  * can hit extra, so want to play something that will match, but might not be obvious
+  * all cards quarter notes, but some rhythms 8th, so need to play multiple cards
+* Rhythm + Tower Defense discussed with David
+  * Increasingly complex patterns
+  * Towers destroy notes
+  * Have to play what makes it through
+  * Only get money for notes played, not destroyed, so if you build too well, you don't get any money
+  * Maybe elemental notes, so you can't destroy everything (would this actually help?)
+* Idle + Golf?
+  * Unlock different clubs with different distances
+  * Unlock auto-clicking of clubs (using a club always leaves you at least as close, but possibly on the other side)
+  * If randomness, unlock multiple balls?
+  * Proceed to larger and larger courses with bigger rewards
+  * Scaling how often clubs are hit, or how much distance they travel?
+  * Is distance left just HP, or is there a physics-game feel to it? How to do physics if players aren't aiming?
+* Programming + ?
---
+++ src/client/app.js
@@ -0,0 +1,284 @@
+/*jshint noempty:false*/
+
+/*global $: false */
+/*global math_device: false */
+/*global assert: false */
+/*global Z: false */
+
+const local_storage = require('./local_storage.js');
+local_storage.storage_prefix = 'turbulenz-playground';
+window.Z = window.Z || {};
+Z.BACKGROUND = 0;
+Z.SPRITES = 10;
+
+// Virtual viewport for our game logic
+const game_width = 1280;
+const game_height = 960;
+
+export function main(canvas)
+{
+  const glov_engine = require('./glov/engine.js');
+  const glov_font = require('./glov/font.js');
+
+  glov_engine.startup({
+    canvas,
+    game_width,
+    game_height,
+    pixely: true,
+  });
+
+  const sound_manager = glov_engine.sound_manager;
+  // const glov_camera = glov_engine.glov_camera;
+  const glov_input = glov_engine.glov_input;
+  const glov_sprite = glov_engine.glov_sprite;
+  const glov_ui = glov_engine.glov_ui;
+  const draw_list = glov_engine.draw_list;
+  // const font = glov_engine.font;
+
+
+  const loadTexture = glov_sprite.loadTexture.bind(glov_sprite);
+  const createSprite = glov_sprite.createSprite.bind(glov_sprite);
+
+  glov_ui.bindSounds(sound_manager, {
+    button_click: 'button_click',
+    rollover: 'rollover',
+  });
+
+  let edit_box = glov_ui.createEditBox({
+    x: 300,
+    y: 100,
+    w: 200,
+  });
+
+  const color_white = math_device.v4Build(1, 1, 1, 1);
+  const color_red = math_device.v4Build(1, 0, 0, 1);
+  const color_yellow = math_device.v4Build(1, 1, 0, 1);
+
+  // Cache key_codes
+  const key_codes = glov_input.key_codes;
+  const pad_codes = glov_input.pad_codes;
+
+  let game_state;
+
+  let sprites = {};
+  const spriteSize = 64;
+  function initGraphics() {
+    if (sprites.white) {
+      return;
+    }
+
+    sound_manager.loadSound('test');
+    loadTexture('test.png');
+
+    sprites.white = createSprite('white', {
+      width : 1,
+      height : 1,
+      x : 0,
+      y : 0,
+      rotation : 0,
+      color : math_device.v4Build(1,1,1,1),
+      origin: math_device.v2Build(0, 0),
+      textureRectangle : math_device.v4Build(0, 0, 1, 1)
+    });
+
+    sprites.test = createSprite('test.png', {
+      width : spriteSize,
+      height : spriteSize,
+      rotation : 0,
+      color : [1,1,1,1],
+      textureRectangle : math_device.v4Build(0, 0, spriteSize, spriteSize)
+    });
+
+    sprites.game_bg = createSprite('white', {
+      width : game_width,
+      height : game_height,
+      x : 0,
+      y : 0,
+      rotation : 0,
+      color : [0, 0.72, 1, 1],
+      origin: [0, 0],
+      textureRectangle : math_device.v4Build(0, 0, spriteSize, spriteSize)
+    });
+  }
+
+  function doBlurEffect(src, dest) {
+    glov_engine.effects.applyGaussianBlur({
+      source: src,
+      destination: dest,
+      blurRadius: 5,
+      blurTarget: glov_engine.getTemporaryTarget(),
+    });
+  }
+  function doDesaturateEffect(src, dest) {
+    let saturation = 0.1;
+
+    // Perf note: do not allocate these each frame for better perf
+    let xform = math_device.m43BuildIdentity();
+    let tmp = math_device.m43BuildIdentity();
+
+    math_device.m43BuildIdentity(xform);
+    if (saturation !== 1) {
+      glov_engine.effects.saturationMatrix(saturation, tmp);
+      math_device.m43Mul(xform, tmp, xform);
+    }
+    // if ((hue % (Math.PI * 2)) !== 0) {
+    //   glov_engine.effects.hueMatrix(hue, tmp);
+    //   math_device.m43Mul(xform, tmp, xform);
+    // }
+    // if (contrast !== 1) {
+    //   glov_engine.effects.contrastMatrix(contrast, tmp);
+    //   math_device.m43Mul(xform, tmp, xform);
+    // }
+    // if (brightness !== 0) {
+    //   glov_engine.effects.brightnessMatrix(brightness, tmp);
+    //   math_device.m43Mul(xform, tmp, xform);
+    // }
+    // if (additiveRGB[0] !== 0 || additiveRGB[1] !== 0 || additiveRGB[2] !== 0) {
+    //   glov_engine.effects.additiveMatrix(additiveRGB, tmp);
+    //   math_device.m43Mul(xform, tmp, xform);
+    // }
+    // if (grayscale) {
+    //   glov_engine.effects.grayScaleMatrix(tmp);
+    //   math_device.m43Mul(xform, tmp, xform);
+    // }
+    // if (negative) {
+    //   glov_engine.effects.negativeMatrix(tmp);
+    //   math_device.m43Mul(xform, tmp, xform);
+    // }
+    // if (sepia) {
+    //   glov_engine.effects.sepiaMatrix(tmp);
+    //   math_device.m43Mul(xform, tmp, xform);
+    // }
+    glov_engine.effects.applyColorMatrix({
+      colorMatrix: xform,
+      source: src,
+      destination: dest,
+    });
+  }
+
+  function test(dt) {
+
+    if (glov_ui.modal_dialog) {
+      // Testing effects
+      glov_engine.queueFrameEffect(Z.MODAL - 2, doBlurEffect);
+      glov_engine.queueFrameEffect(Z.MODAL - 1, doDesaturateEffect);
+    }
+
+    if (!test.color_sprite) {
+      test.color_sprite = math_device.v4Copy(color_white);
+      test.character = {
+        x : (Math.random() * (game_width - spriteSize) + (spriteSize * 0.5)),
+        y : (Math.random() * (game_height - spriteSize) + (spriteSize * 0.5)),
+      };
+    }
+
+    test.character.dx = 0;
+    test.character.dy = 0;
+    if (glov_input.isKeyDown(key_codes.LEFT) || glov_input.isKeyDown(key_codes.A) || glov_input.isPadButtonDown(0, pad_codes.LEFT)) {
+      test.character.dx = -1;
+    } else if (glov_input.isKeyDown(key_codes.RIGHT) || glov_input.isKeyDown(key_codes.D) || glov_input.isPadButtonDown(0, pad_codes.RIGHT)) {
+      test.character.dx = 1;
+    }
+    if (glov_input.isKeyDown(key_codes.UP) || glov_input.isKeyDown(key_codes.W) || glov_input.isPadButtonDown(0, pad_codes.UP)) {
+      test.character.dy = -1;
+    } else if (glov_input.isKeyDown(key_codes.DOWN) || glov_input.isKeyDown(key_codes.S) || glov_input.isPadButtonDown(0, pad_codes.DOWN)) {
+      test.character.dy = 1;
+    }
+
+    test.character.x += test.character.dx * dt * 0.2;
+    test.character.y += test.character.dy * dt * 0.2;
+    let bounds = {
+      x: test.character.x - spriteSize/2,
+      y: test.character.y - spriteSize/2,
+      w: spriteSize,
+      h: spriteSize,
+    };
+    if (glov_input.isMouseDown() && glov_input.isMouseOver(bounds)) {
+      math_device.v4Copy(color_yellow, test.color_sprite);
+    } else if (glov_input.clickHit(bounds)) {
+      math_device.v4Copy((test.color_sprite === color_red) ? color_white : color_red, test.color_sprite);
+      sound_manager.play('test');
+    } else if (glov_input.isMouseOver(bounds)) {
+      math_device.v4Copy(color_white, test.color_sprite);
+      test.color_sprite[3] = 0.5;
+    } else {
+      math_device.v4Copy(color_white, test.color_sprite);
+      test.color_sprite[3] = 1;
+    }
+
+    draw_list.queue(sprites.game_bg, 0, 0, Z.BACKGROUND, [0, 0.72, 1, 1]);
+    draw_list.queue(sprites.test, test.character.x, test.character.y, Z.SPRITES, test.color_sprite, null, null, 0, 'alpha');
+
+    let font_test_idx = 0;
+
+    glov_ui.print(glov_font.styleColored(null, 0x000000ff), test.character.x, test.character.y + (++font_test_idx * 20), Z.SPRITES,
+      'TEXT!');
+    let font_style = glov_font.style(null, {
+      outline_width: 1.0,
+      outline_color: 0x800000ff,
+      glow_xoffs: 3.25,
+      glow_yoffs: 3.25,
+      glow_inner: -2.5,
+      glow_outer: 5,
+      glow_color: 0x000000ff,
+    });
+    glov_ui.print(font_style, test.character.x, test.character.y + (++font_test_idx * glov_ui.font_height), Z.SPRITES,
+      'Outline and Drop Shadow');
+
+    if (glov_ui.buttonText({ x: 100, y: 100, text: 'Button!'})) {
+      glov_ui.modalDialog({
+        title: 'Modal Dialog',
+        text: 'This is a modal dialog!',
+        buttons: {
+          'OK': function () {
+            console.log('OK pushed!');
+          },
+          'Cancel': null, // no callback
+        },
+      });
+    }
+
+    if (edit_box.run() === edit_box.SUBMIT) {
+      glov_ui.modalDialog({
+        title: 'Modal Dialog',
+        text: `Edit box submitted with text ${edit_box.text}`,
+        buttons: {
+          'OK': null,
+        },
+      });
+    }
+    glov_ui.print(font_style, 300, 140, Z.UI, `Edit Box Text: ${edit_box.text}`);
+  }
+
+  function testInit(dt) {
+    $('.screen').hide();
+    $('#title').show();
+    game_state = test;
+    test(dt);
+  }
+
+  function loading() {
+    let load_count = glov_sprite.loading() + sound_manager.loading();
+    $('#loading').text(`Loading (${load_count})...`);
+    if (!load_count) {
+      game_state = testInit;
+    }
+  }
+
+  function loadingInit() {
+    initGraphics();
+    $('.screen').hide();
+    $('#title').show();
+    game_state = loading;
+    loading();
+  }
+
+  game_state = loadingInit;
+
+  function tick(dt) {
+    game_state(dt);
+  }
+
+  loadingInit();
+  glov_engine.go(tick);
+}
